7eab3a104e035db523ececf68ecd5c4592c1bcec
victor-sc12 <victorsuquilanda8639@outlook.com>
2026-05-24 14:01:32 -0500

    Habilita el motor Transit de Vault y crea la clave clickloker-key para cifrado entre sistemas.

---
+++ init-vault.sh
@@ -0,0 +1,14 @@
+# infraestructura/init-vault.sh
+
+echo "Esperando que Vault esté listo..."
+sleep 8
+
+echo "Habilitando motor Transit..."
+docker exec -e VAULT_ADDR=http://127.0.0.1:8200 -e VAULT_TOKEN=vault_root_token vault vault secrets enable transit
+
+echo "Creando llave clickloker-key..."
+docker exec -e VAULT_ADDR=http://127.0.0.1:8200 -e VAULT_TOKEN=vault_root_token vault vault write -f transit/keys/clickloker-key
+
+echo "Vault listo."
+
+## pa' ejecutar, usar: bash init-vault.sh
