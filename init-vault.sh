861a979d85cc3444a3505ee60e50acb607687fc9
victor-sc12 <victorsuquilanda8639@outlook.com>
2026-07-04 12:25:02 -0500

    Adición de nueva llave Vault

--- init-vault.sh
+++ init-vault.sh
@@ -9,6 +9,9 @@ docker exec -e VAULT_ADDR=http://127.0.0.1:8200 -e VAULT_TOKEN=vault_root_token 
 echo "Creando llave clickloker-key..."
 docker exec -e VAULT_ADDR=http://127.0.0.1:8200 -e VAULT_TOKEN=vault_root_token vault vault write -f transit/keys/clickloker-key
 
+echo "Creando llave plataforma-key..."
+docker exec -e VAULT_ADDR=http://127.0.0.1:8200 -e VAULT_TOKEN=vault_root_token vault vault write -f transit/keys/plataforma-key
+
 echo "Vault listo."
 
 ## pa' ejecutar, usar: bash init-vault.sh
